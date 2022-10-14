e8d4ead7bf70064348b30fb078066f04a0fbfbe2
Roberto <30988803+Hobyn@users.noreply.github.com>
2022-10-14 15:38:57 -0300

    Update main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite/'
+      config.base = '/ignit/'
     }
 
     return config
